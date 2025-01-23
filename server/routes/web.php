6eb21855d1cf19c12d3ac6f6f29f34287db69129
Herberts Eisulis <h.eisulis@gmail.com>
2025-01-24 00:16:40 +0200

    set up backend to properly work. installed axios package to the client

--- server/routes/web.php
+++ server/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Welcome";
+});
+
+Route::get("/register", function() {
+    return "Register";
 });
