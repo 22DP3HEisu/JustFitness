7fe6a81692312ac3d22bdb9042873bf554fee2c8
22DP3HEisu <he00169@rvt.lv>
2025-02-22 02:07:08 +0200

    fixed last detached head branch problems

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import './index.css';
@@ -7,15 +7,17 @@ import Start from './routes/start';
 import Login from './routes/login';
 import SignUp from './routes/signup';
 import axios from './lib/axios';
-import UserContext from './contexts/UserContext';
 
 export function Header() {
-  const {user, setUser} = useContext(UserContext);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     if (window.localStorage.getItem("token")) {
       console.log("User is logged in");
       
+      axios.get("/user").then(({data}) => {
+        setUser(data)
+      })
       console.log(user);
     }
   }, [])
